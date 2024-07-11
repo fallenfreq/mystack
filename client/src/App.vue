20cd6f77335d4084bb20dec4790fa98885774a4e
fallenfreq <michael@catsdog.co.uk>
2024-07-11 19:10:17 +0100

    Added Vuestic example

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/test-form">Vuestic</RouterLink>
         <RouterLink to="/admin" v-if="$zitadel.hasRole('admin')">Admin</RouterLink>
         <a
           href="#"
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -6,6 +6,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/test-form',
+      name: 'testForm',
+      component: () => import('../views/TestForm.vue')
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView
