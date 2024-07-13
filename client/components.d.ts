e546fe40bc30258354bd93469507e044eb34e75d
fallenfreq <michaelhigginson@hotmail.co.uk>
2024-07-14 00:33:57 +0100

    Moved nav to its own component

--- client/components.d.ts
+++ client/components.d.ts
@@ -18,6 +18,7 @@ declare module 'vue' {
     IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     InputText: typeof import('primevue/inputtext')['default']
+    NavBar: typeof import('./src/components/NavBar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SelectButton: typeof import('primevue/selectbutton')['default']
