927b76c89b97e7ceafa545ebfbbb4292d0d17957
fallenfreq <michael@catsdog.co.uk>
2024-07-14 21:11:06 +0100

    Restyling

--- client/src/views/NoAccess.vue
+++ client/src/views/NoAccess.vue
@@ -1,22 +1,6 @@
 <template>
-  <div class="admin">
-    <div>
-    <h1>
-      Access denied
-    </h1>
-    <h2>
-      You don't have the role "admin" in your ZITADEL project.
-    </h2>
-    </div>
+  <div>
+    <h1>Access denied</h1>
+    <h2>You don't have the role "admin" in your ZITADEL project.</h2>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .admin {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
