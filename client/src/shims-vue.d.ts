1d3e77140a1f9960a143b33f9e1640563582b6b7
fallenfreq <michael@catsdog.co.uk>
2024-07-24 00:48:27 +0100

    Removed redundant shims

--- client/src/shims-vue.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ComponentCustomProperties } from 'vue'
-
-declare module '@vue/runtime-core' {
-  // Declare augmentation for the `ComponentCustomProperties` interface
-  interface ComponentCustomProperties {
-    $addKeyCombo: (keyCombo: string, func: () => void) => void
-    $removeKeyCombo: (keyCombo: string) => void
-  }
-}
